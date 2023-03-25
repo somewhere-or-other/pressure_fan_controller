0b72d698e7ff2a2e097b523137f6b9722c5fe485
Lloyd Brown <somewhere.or.other@gmail.com>
2023-03-25 15:21:47 -0600

    Verifying/fixing mapping between pressure and potentiometer setpoint

--- pressure_fan_controller.ino
+++ pressure_fan_controller.ino
@@ -43,29 +43,52 @@ void setPotentiometer(int set) {
   ds3502.setWiper(set);
 }
 
-void setup() {
-  Serial.begin(115200);
-  // Wait until serial port is opened
-  while (!Serial) { delay(1); }
+void outputMapping() {
+  for (float p=PRESSUREMIN; p<=PRESSUREMAX; p+=0.1) {
+    int setting=getPotentiometerSetting(p);
+    Serial.print("(pressure, potentiometersetting) = (");
+    Serial.print(p,2);
+    Serial.print(", ");
+    Serial.print(setting);
+    Serial.println(")");
+  }
 
-  potentiometerSetup();
-  sdpSetup();
-
+  delay(10000);
 }
+
 
 int floatToIntMap(float x, float in_min, float in_max, int out_min, int out_max) {
   return (x - in_min) * ((float)out_max - (float)out_min) / (in_max - in_min) + (float)out_min;
 }
 
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 int getPotentiometerSetting(float differentialPressure) {
-  if ((differentialPressure-PRESSURESETPOINT) <= PRESSURETOLERANCE) { // If pressure is at or below PRESSURESETPOINT (within PRESSURETOLERANCE)
+  if (differentialPressure < (PRESSUREMIN+PRESSURETOLERANCE)) { // If pressure is at or below PRESSURESETPOINT (within PRESSURETOLERANCE)
     return FANMIN;
-  } else if (differentialPressure > (PRESSURESETPOINT-PRESSURETOLERANCE)) {
+  } else if (differentialPressure > (PRESSUREMAX-PRESSURETOLERANCE)) {
     return FANMAX;
   } else {
     // TODO: this is where to integrate the PID controller mapping
-    return floatToIntMap(differentialPressure, PRESSUREMIN, PRESSUREMAX, FANMIN, FANMAX);
+    return floatMap(differentialPressure, PRESSUREMIN, PRESSUREMAX, FANMIN, FANMAX);
   }
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  // Wait until serial port is opened
+  while (!Serial) { delay(1); }
+
+  //outputMapping(); //To output the current map, as debugging verification
+
+  potentiometerSetup();
+  sdpSetup();
+
 }
 
 void loop() {
