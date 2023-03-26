3a9077843da4fb3fc61a24f5c5f87fb2ddba108c
Lloyd Brown <somewhere.or.other@gmail.com>
2023-03-25 22:16:52 -0600

    basic procedural framework (hopefully cleaner)

--- pressure_fan_controller.ino
+++ pressure_fan_controller.ino
@@ -1,6 +1,18 @@
-#include <LiquidCrystal_I2C.h>
+#define FANMIN 0
+#define FANMAX 127
+#define PRESSURETOLERANCE 2.0
+#define PRESSURESETPOINT 0.0
+#define PRESSUREMIN 0.0
+#define PRESSUREMAX 100.0 //corresponds to maximum fan output
 
-LiquidCrystal_I2C display = LiquidCrystal_I2C(0x27, 20, 4); //default 0x27 I2C address, 20 columns, 4 rows
+#define LCD_I2C_ADDR 0x27
+#define LCD_COLUMNS 20
+#define LCD_ROWS 4
+
+
+//for display
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C display = LiquidCrystal_I2C(LCD_I2C_ADDR, LCD_COLUMNS, LCD_ROWS); 
 
 //For pressure sensor
 #include <Wire.h>
@@ -9,15 +21,36 @@ SDP8XXSensor sdp;
 
 //For potentiometer
 #include <Adafruit_DS3502.h>
-Adafruit_DS3502 ds3502 = Adafruit_DS3502();
+Adafruit_DS3502 potentiometer = Adafruit_DS3502();
+
+
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+int getPotentiometerSetting(float differentialPressure) {
+  if (differentialPressure < (PRESSUREMIN+PRESSURETOLERANCE)) { // If pressure is at or below PRESSURESETPOINT (within PRESSURETOLERANCE)
+    return FANMIN;
+  } else if (differentialPressure > (PRESSUREMAX-PRESSURETOLERANCE)) {
+    return FANMAX;
+  } else {
+    // TODO: this is where to integrate the PID controller mapping
+    return floatMap(differentialPressure, PRESSUREMIN, PRESSUREMAX, FANMIN, FANMAX);
+  }
+}
 
 void potentiometerSetup() {
-  if (!ds3502.begin()) {
-    Serial.println("Couldn't find DS3502 chip");
+  if (!potentiometer.begin()) {
+    Serial.println("Couldn't find potentiometer chip");
     while (1);
   }
 }
 
+void potentiometerSet(uint8_t setting) {
+  potentiometer.setWiper(setting);
+}
 
 void pressureSensorSetup() {
   Wire.begin();
@@ -32,15 +65,57 @@ float pressureSensorGet() {
   }
 }
 
+void displaySetup() {
+  display.backlight(); //turn on backlight
+  display.clear();
 
-void setup() {
-  pressureSensorSetup();
-  potentiometerSetup();
+  display.setCursor(0,0);
+  display.print("Fan Speed Controller");
+}
+
+void displayToSerial(float pressure, uint8_t potentiometerSetting) {
+  Serial.print("Measured_pressure: ");
+  Serial.print(pressure);
+  Serial.print(" pa | potentiometerSetting: ");
+  Serial.println(potentiometerSetting);
+}
+
+void displayToLCD(float pressure, uint8_t potentiometerSetting) {
+  char buffer[LCD_COLUMNS];
+  snprintf(buffer, LCD_COLUMNS, "Pressure: %0.2f pa", pressure);
+  display.setCursor(1,0);
+  display.print(buffer);
+
+  snprintf(buffer, LCD_COLUMNS, "Potentiometer: %u", potentiometerSetting);
+  display.setCursor(2,0);
+  display.print(buffer);
   
 }
+void setup() {
+  Serial.begin(115200);
+  // Wait until serial port is opened
+  while (!Serial) { delay(1); }
+
+  pressureSensorSetup();
+  potentiometerSetup();
+
+}
+
+
 
 void loop() {
 
+  float pressure = pressureSensorGet();
+  float potentiometerSetting = getPotentiometerSetting(pressure);
+
+  potentiometerSet(potentiometerSetting);
+
+  displayToSerial(pressure, potentiometerSetting);
+  displayToLCD(pressure, potentiometerSetting);
+  
+  
+
+  delay(1000);
   
 
 } 
