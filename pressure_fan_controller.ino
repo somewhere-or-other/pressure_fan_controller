19d2e851d90f9a444ee748d01615c718f4efd2d8
Lloyd Brown <somewhere.or.other@gmail.com>
2023-03-25 21:44:12 -0600

    not OOP, but a basic start

--- pressure_fan_controller.ino
+++ pressure_fan_controller.ino
@@ -1,6 +1,42 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C display = LiquidCrystal_I2C(0x27, 20, 4); //default 0x27 I2C address, 20 columns, 4 rows
+
+//For pressure sensor
+#include <Wire.h>
+#include <sdpsensor.h>
+SDP8XXSensor sdp;
+
+//For potentiometer
+#include <Adafruit_DS3502.h>
+Adafruit_DS3502 ds3502 = Adafruit_DS3502();
+
+void potentiometerSetup() {
+  if (!ds3502.begin()) {
+    Serial.println("Couldn't find DS3502 chip");
+    while (1);
+  }
+}
+
+
+void pressureSensorSetup() {
+  Wire.begin();
+}
+
+float pressureSensorGet() {
+  int ret = sdp.readSample();
+  if (ret == 0) {
+    return sdp.getDifferentialPressure();
+  } else {
+    return NULL;
+  }
+}
+
 
 void setup() {
-
+  pressureSensorSetup();
+  potentiometerSetup();
+  
 }
 
 void loop() {
